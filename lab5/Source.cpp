6ead3f377af90838d89a784e086e5a227ccb5571
COTOPESq <127554013+COTOPESq@users.noreply.github.com>
2024-03-02 10:23:41 +0300

    work version

---
+++ lab5/Source.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Student {
+public:
+    string name;
+    string group;
+    int studentId;
+    int grades[4];
+
+public:
+    Student() {}
+
+    Student(const string& name, const string& group, int studentId, const int* grades) : name(name), group(group), studentId(studentId) {
+        for (int i = 0; i < 4; ++i) {
+            this->grades[i] = grades[i];
+        }
+    }
+
+    friend ostream& operator<<(ostream& os, const Student& student) {
+        os << "Name: " << student.name << ", Group: " << student.group << ", Student ID: " << student.studentId << ", Grades: ";
+        for (int i = 0; i < 4; ++i) {
+            os << student.grades[i] << " ";
+        }
+        return os;
+    }
+};
+
+class hash_fun {
+public:
+    size_t operator()(const Student& student) const {
+        size_t fio = 0;
+        for (auto sim : student.name) {
+            fio += (size_t)sim;
+        }
+        fio += student.studentId;
+        return(fio);
+    }
+};
+
+bool operator == (const Student& student1, const Student& student2) {
+    return (student1.name == student2.name);
+}
+
+bool operator < (const Student& student1, const Student& student2) {
+    return (student1.name < student2.name);
+}
+
+
+int main() {
+    string name, group;
+    int studentId, grades[4];
+    set <Student> StudentSet;
+    unordered_set <Student, hash_fun> StundetUnd;
+
+    ifstream inputFile("input.txt");
+    if (!inputFile) {
+        cerr << "Unable to open input file" << endl;
+        return 1;
+    }
+
+    while (inputFile >> name >> group >> studentId >> grades[0] >> grades[1] >> grades[2] >> grades[3]) {
+        StudentSet.insert(Student(name, group, studentId, grades));
+        StundetUnd.insert(Student(name, group, studentId, grades));
+    }
+    
+    ofstream outputFile("output.txt");
+    if (!outputFile) {
+        cerr << "Unable to open output file" << endl;
+        return 1;
+    }
+    
+    outputFile << "Set container\n";
+    for (const auto& student : StudentSet) {
+        outputFile << student << endl;
+    }
+
+
+    outputFile << "Unord container\n";
+    for (const auto& student : StundetUnd) {
+        outputFile << student << endl;
+    }
+    return 0;
+}
